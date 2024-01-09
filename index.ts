934ee0f0e4db25f5638ae039a1a1853225fdead7
Manoj Santra <santramanoj1997@gmail.com>
2024-01-09 10:54:28 +0530

    graphql server with express middleware created

--- index.ts
+++ index.ts
@@ -1,16 +1,72 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
-import express from 'express';
+import { ApolloServer, BaseContext } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express, {
+  Application,
+  Request,
+  Response,
+  json,
+  urlencoded,
+} from "express";
+import http from "http";
+import cors from "cors";
+import { config } from "dotenv";
+import bodyParser from "body-parser";
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
 
-const app = express();
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
 
-const server = new ApolloServer<any>({
-  typeDefs:``,
-  resolvers:()=>{},
-});
-// Note you must call `start()` on the `ApolloServer`
-// instance before passing the instance to `expressMiddleware`
-server.start();
+class ExpressApp {
+  private app: Application;
+  private PORT: unknown;
+  private httpServer: http.Server<
+    typeof http.IncomingMessage,
+    typeof http.ServerResponse
+  >;
+  private apolloServer: ApolloServer<BaseContext>;
+  constructor() {
+    config();
+    this.app = express();
+    this.PORT = process.env.PORT ?? 5000;
+    this.httpServer = http.createServer(this.app);
+    this.apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer: this.httpServer }),
+      ],
+    });
+    this.middleware();
+    
+    
+  }
+  private async middleware() {
+    await this.apolloServer.start();
+    this.app.use(
+      "/graphql",
+      cors(),
+      express.json(),
+      expressMiddleware(this.apolloServer)
+    );
+  }
+  public async listen() {
+    try {
+      await this.httpServer.listen({ port: this.PORT });
+      console.log(`server listening at ${this.PORT}`);
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  }
+}
+const server = new ExpressApp();
+server.listen();
 
-app.use('/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(server));
